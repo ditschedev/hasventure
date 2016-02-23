c9d7407ec8d609caf6c7cf175c9c12c38222dfa6
txbivs <tobi.dittmann99@gmail.com>
2016-02-23 08:53:05 +0100

    added configs

--- src/hasventure/gui.java
+++ src/hasventure/gui.java
@@ -5,12 +5,69 @@
  */
 package hasventure;
 
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.Timer;
+
 /**
  *
  * @author tobias.dittmann
  */
 public class gui {
-    public gui(){
-        
-    }
+    Timer time;
+	Image img;
+	int key;
+	int nx,nx2;
+	
+	
+	int figur_y = 235;
+
+			
+	int X_Bild;
+	int lauf;
+	
+	int anzahl = 0;
+	int anzahl2= 0;
+	
+	
+		Image img2;
+		int left = 75;
+
+	public gui(){
+		
+		
+
+		
+		nx = 0;
+		nx2 = 690;
+		
+		key = 0;
+		lauf = 0;
+		
+		setFocusable(true);
+		
+		
+		ImageIcon u = new ImageIcon((getClass().getResource("background.jpg")));
+		img = u.getImage();
+		
+		
+		ImageIcon s = new ImageIcon((getClass().getResource("char.png")));
+		
+		img2 = s.getImage();
+		
+	
+		addKeyListener(new AL());
+		
+		//neu
+		//149 y hoch Springen - Höhe 
+		//block1 = new Block(250, 110, 50, 50, Color.YELLOW);
+		//
+		
+		Sprung sprung = new Sprung();
+	
+		
+		time = new Timer(5,this);
+		time.start();
+		
+	}
 }
